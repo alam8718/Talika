eb2e77ce4bcd4a432c895988aef8ca914bc72675
Mahabub Alam <mahabubalam3004@gmail.com>
2023-11-03 18:53:17 +0600

    title change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const addTalika = (todo) => {
-    setTodos((prev) => [{id: Date.now(),...todo}, ...prev]);
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev]);
   };
   const updateTalika = (id, todo) => {
     setTodos((prev) =>
@@ -51,17 +51,17 @@ function App() {
       value={{todos, addTalika, updateTalika, deleteTalika, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
+          <h1 className="text-4xl  sm:text-7xl uppercase tracking-[60px]  font-bold text-center mb-8 mt-2">
+            Talika
           </h1>
           <div className="mb-4">
             {/* Todo form goes here */}
             <TalikaForm />
-            </div>
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
-            {todos.map((todo)=>(
-              <div keys={todo.id} className="w-full" >
+            {todos.map((todo) => (
+              <div keys={todo.id} className="w-full">
                 <TalikaItem todo={todo} />
               </div>
             ))}
